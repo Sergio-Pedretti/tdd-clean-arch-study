8604f057d2ae4e6f017ca7bef1fe875450470ee9
Sergio-Pedretti <sergiopedretti07@gmail.com>
2022-11-01 21:51:39 -0300

    ensure AccessToken has a value

---
+++ tests/domain/models/access-tokens.spec.ts
@@ -0,0 +1,8 @@
+import { AccessToken } from '@/domain/models'
+
+describe('AccessToken', () => {
+  it('should create with a value', () => {
+    const sut = new AccessToken('any-value')
+    expect(sut).toEqual({ value: 'any-value' })
+  })
+})
