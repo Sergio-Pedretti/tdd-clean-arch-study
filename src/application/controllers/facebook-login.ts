fd9d46cd04e7a695ddd2aa00df7523ff14305304
Sergio-Pedretti <sergiopedretti07@gmail.com>
2022-11-12 10:38:56 -0300

    extract validation logic into new method

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -4,7 +4,7 @@ import { HttpResponse, badRequest, unauthorized, serverError, ok } from '@/appli
 import { RequiredField } from '@/application/erros'
 
 type HttpRequest = {
-  token: string | undefined | null
+  token: string
 }
 
 type Model = Error | { accessToken: string }
@@ -14,9 +14,11 @@ export class FacebookLoginController {
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse<Model>> {
     try {
-      if (httpRequest.token === '' || httpRequest.token === undefined || httpRequest.token === null) {
-        return badRequest(new RequiredField('token'))
+      const error = this.validate(httpRequest)
+      if (error !== undefined) {
+        return badRequest(error)
       }
+
       const accessToken = await this.facebookAuth.perform({ token: httpRequest.token })
       if (accessToken instanceof AccessToken) {
         return ok({ accessToken: accessToken.value })
@@ -24,6 +26,12 @@ export class FacebookLoginController {
       return unauthorized()
     } catch (error) {
       return serverError(error as Error)
+    }
+  }
+
+  private validate (httpRequest: HttpRequest): Error | undefined {
+    if (httpRequest.token === '' || httpRequest.token === undefined || httpRequest.token === null) {
+      return new RequiredField('token')
     }
   }
 }
