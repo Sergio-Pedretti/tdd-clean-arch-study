773102e468b37bdad9f705ec9a6f48b6912cfc34
Sergio-Pedretti <sergiopedretti07@gmail.com>
2023-01-10 21:44:17 -0300

    move AuthenticationMiddleware to production

---
+++ src/application/middlewares/authentication.ts
@@ -0,0 +1,24 @@
+import { forbidden, HttpResponse, ok } from '@/application/helpers'
+import { RequiredStringValidator } from '@/application/validation'
+import { Authorize } from '@/domain/use-cases'
+
+type HttpRequest = { authorization: string }
+type Model = Error | { userId: string }
+
+export class AuthenticationMiddleware {
+  constructor (private readonly authorize: Authorize) {}
+  async handle ({ authorization }: HttpRequest): Promise<HttpResponse<Error | Model>> {
+    if (!this.validate({ authorization })) return forbidden()
+    try {
+      const userId = await this.authorize({ token: authorization })
+      return ok({ userId })
+    } catch {
+      return forbidden()
+    }
+  }
+
+  validate ({ authorization }: HttpRequest): boolean {
+    const error = new RequiredStringValidator(authorization, 'authorization').validate()
+    return error === undefined
+  }
+}
