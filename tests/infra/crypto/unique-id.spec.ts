03a4a9641447a480dd10f697e3d57e1d503fe8e1
Sergio-Pedretti <sergiopedretti07@gmail.com>
2023-01-22 17:11:22 -0300

    move UniqueId to its own file

--- tests/infra/crypto/unique-id.spec.ts
+++ tests/infra/crypto/unique-id.spec.ts
@@ -1,18 +1,4 @@
-import { UUIDGenerator } from '@/domain/contracts/crypto'
-
-class UniqueId {
-  constructor (private readonly date: Date) {}
-  uuid ({ key }: UUIDGenerator.Input): UUIDGenerator.Output {
-    return key +
-    '-' +
-    this.date.getFullYear().toString() +
-    (this.date.getMonth() + 1).toString().padStart(2, '0') +
-    this.date.getDate().toString().padStart(2, '0') +
-    this.date.getHours().toString().padStart(2, '0') +
-    this.date.getMinutes().toString().padStart(2, '0') +
-    this.date.getSeconds().toString().padStart(2, '0')
-  }
-}
+import { UniqueId } from '@/infra/crypto'
 
 describe('UniqueId', () => {
   it('should call unique uuid', () => {
