8dc53ee35f980fc8e008ddf6c8dbe21366faffe1
Sergio-Pedretti <sergiopedretti07@gmail.com>
2023-03-13 11:45:58 -0300

    add Required Validator

---
+++ tests/application/controllers/validation/required.spec.ts
@@ -0,0 +1,48 @@
+import { RequiredField } from '@/application/erros'
+import { Required, RequiredStringValidator } from '@/application/validation'
+
+describe('Required', () => {
+  it('should return error if value is null', () => {
+    const sut = new Required(null as any, 'any-field')
+
+    const error = sut.validate()
+
+    expect(error).toEqual(new RequiredField('any-field'))
+  })
+  it('should return error if value is undefined', () => {
+    const sut = new Required(undefined as any, 'any-field')
+
+    const error = sut.validate()
+
+    expect(error).toEqual(new RequiredField('any-field'))
+  })
+  it('should return undefined if value is defined', () => {
+    const sut = new Required('any-value', 'any-field')
+
+    const error = sut.validate()
+
+    expect(error).toBeUndefined()
+  })
+})
+
+describe('RequiredStringValidator', () => {
+  it('should extend Required', () => {
+    const sut = new RequiredStringValidator('')
+
+    expect(sut).toBeInstanceOf(Required)
+  })
+  it('should return error if value is empty', () => {
+    const sut = new RequiredStringValidator('', 'any-field')
+
+    const error = sut.validate()
+
+    expect(error).toEqual(new RequiredField('any-field'))
+  })
+  it('should return undefined if value is defined', () => {
+    const sut = new RequiredStringValidator('any-value', 'any-field')
+
+    const error = sut.validate()
+
+    expect(error).toBeUndefined()
+  })
+})
