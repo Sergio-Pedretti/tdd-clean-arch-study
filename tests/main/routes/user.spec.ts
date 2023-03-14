41d3ae16bfadf295640a6e1f5d14dcf2227ad1e5
Sergio-Pedretti <sergiopedretti07@gmail.com>
2023-03-14 12:23:03 -0300

    ensure picture route returns 403 if no auth header is present

---
+++ tests/main/routes/user.spec.ts
@@ -0,0 +1,33 @@
+import { app } from '@/main/config/app'
+import { PgUser } from '@/infra/postgres/entities'
+import { makeFakeDb } from '@/tests/infra/postgres/mocks'
+
+import { IBackup } from 'pg-mem'
+import { getConnection } from 'typeorm'
+import request from 'supertest'
+
+describe('User Routes', () => {
+  describe('DELETE /users/picture', () => {
+    let backup: IBackup
+
+    beforeAll(async () => {
+      const db = await makeFakeDb([PgUser])
+      backup = db.backup()
+    })
+
+    beforeEach(() => {
+      backup.restore()
+    })
+
+    afterAll(async () => {
+      await getConnection().close()
+    })
+
+    it('it should return 403 if no authorization header is present', async () => {
+      const { status } = await request(app)
+        .delete('/api/users/picture')
+
+      expect(status).toBe(403)
+    })
+  })
+})
