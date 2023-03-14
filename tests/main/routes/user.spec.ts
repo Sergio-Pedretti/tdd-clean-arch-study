39653cf4a6e4bba5cb5dff42026da219ff025833
Sergio-Pedretti <sergiopedretti07@gmail.com>
2023-03-14 16:10:00 -0300

    fix broken test after last update

--- tests/main/routes/user.spec.ts
+++ tests/main/routes/user.spec.ts
@@ -35,16 +35,16 @@ describe('User Routes', () => {
       expect(status).toBe(403)
     })
 
-    it('it should return 204', async () => {
-      const { id } = await pgUserRepo.save({ email: 'any-email' })
+    it('it should return 200 with valid data', async () => {
+      const { id } = await pgUserRepo.save({ email: 'any-email', name: 'Sérgio pedretti' })
       const authorization = sign({ key: id }, env.jwtSecret)
 
       const { status, body } = await request(app)
         .delete('/api/users/picture')
         .set({ authorization })
 
-      expect(status).toBe(204)
-      expect(body).toEqual({})
+      expect(status).toBe(200)
+      expect(body).toEqual({ pictureUrl: undefined, initials: 'SP' })
     })
   })
 })
