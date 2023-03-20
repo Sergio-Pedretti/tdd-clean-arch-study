40c98632cf88b34e9a5cecdf00ebdac09e84fa02
Sergio-Pedretti <sergiopedretti07@gmail.com>
2023-03-20 09:46:23 -0300

    ensure PgConnection has only one instace

---
+++ tests/infra/postgres/helpers/connection.spec.ts
@@ -0,0 +1,26 @@
+
+export class PgConnection {
+  private static instance?: PgConnection
+
+  private constructor () {}
+
+  static getInstance (): PgConnection {
+    if (PgConnection.instance === undefined) {
+      PgConnection.instance = new PgConnection()
+    }
+    return PgConnection.instance
+  }
+
+  createConnection (): void {
+    console.log('OK')
+  }
+}
+
+describe('PgConnection', () => {
+  it('should have only one instance', () => {
+    const sut = PgConnection.getInstance()
+    const sut2 = PgConnection.getInstance()
+
+    expect(sut).toBe(sut2)
+  })
+})
