429e546de6251f21ef0b1733a4da3c373fe32335
Sergio-Pedretti <sergiopedretti07@gmail.com>
2023-03-27 09:49:31 -0300

    change UniqueId to get new Date every call

--- src/infra/crypto/unique-id.ts
+++ src/infra/crypto/unique-id.ts
@@ -1,15 +1,15 @@
 import { UUIDGenerator } from '@/domain/contracts/crypto'
 
-export class UniqueId {
-  constructor (private readonly date: Date) {}
+export class UniqueId implements UUIDGenerator {
   uuid ({ key }: UUIDGenerator.Input): UUIDGenerator.Output {
+    const date = new Date()
     return key +
     '-' +
-    this.date.getFullYear().toString() +
-    (this.date.getMonth() + 1).toString().padStart(2, '0') +
-    this.date.getDate().toString().padStart(2, '0') +
-    this.date.getHours().toString().padStart(2, '0') +
-    this.date.getMinutes().toString().padStart(2, '0') +
-    this.date.getSeconds().toString().padStart(2, '0')
+    date.getFullYear().toString() +
+    (date.getMonth() + 1).toString().padStart(2, '0') +
+    date.getDate().toString().padStart(2, '0') +
+    date.getHours().toString().padStart(2, '0') +
+    date.getMinutes().toString().padStart(2, '0') +
+    date.getSeconds().toString().padStart(2, '0')
   }
 }
--- tests/infra/crypto/unique-id.spec.ts
+++ tests/infra/crypto/unique-id.spec.ts
@@ -1,16 +1,24 @@
 import { UniqueId } from '@/infra/crypto'
 
+import { set, reset } from 'mockdate'
+
 describe('UniqueId', () => {
-  it('should call unique uuid', () => {
-    const sut = new UniqueId(new Date(2022, 1, 12, 18, 35, 22))
+  afterEach(() => {
+    reset()
+  })
+
+  it('should call unique uuid with Date 2022-02-12 18:35:22', () => {
+    set(new Date(2022, 1, 12, 18, 35, 22))
+    const sut = new UniqueId()
 
     const uuid = sut.uuid({ key: 'any-key' })
 
     expect(uuid).toBe('any-key-20220212183522')
   })
 
-  it('should call unique uuid', () => {
-    const sut = new UniqueId(new Date(2022, 4, 12, 9, 0, 1))
+  it('should call unique uuid with Date 2022-05-12 09:00:01', () => {
+    set(new Date(2022, 4, 12, 9, 0, 1))
+    const sut = new UniqueId()
 
     const uuid = sut.uuid({ key: 'any-key' })
 
