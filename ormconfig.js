026d8e7bfa2a7aa87359ee0cdeae9ae57a59a89c
Sergio-Pedretti <sergiopedretti07@gmail.com>
2023-03-27 11:41:04 -0300

    fix ormconfig connection to elephantSql

--- ormconfig.js
+++ ormconfig.js
@@ -1,9 +1,7 @@
 module.exports = {
   type: process.env.DB_NAME,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB,
+  url: process.env.DB_URL,
+  synchronize: true,
+  logging: true,
   entities: [`${process.env.TS_NODE_DEV === undefined ? 'dist' : 'src'}/infra/postgres/entities/index.{js,ts}`]
 }
