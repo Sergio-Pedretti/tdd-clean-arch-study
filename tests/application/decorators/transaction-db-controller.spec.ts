c64a8e50ef5d0327aa7e112f6fd3e996ebf8fe36
Sergio-Pedretti <sergiopedretti07@gmail.com>
2023-03-27 13:59:08 -0300

    ensure DbTransactionController executes decoratee

--- tests/application/decorators/transaction-db-controller.spec.ts
+++ tests/application/decorators/transaction-db-controller.spec.ts
@@ -1,9 +1,15 @@
+import { Controller } from '@/application/controllers'
 import { mock, MockProxy } from 'jest-mock-extended'
 
 export class DbTransactionController {
-  constructor (private readonly db: DbTransaction) {}
+  constructor (
+    private readonly decoratee: Controller,
+    private readonly db: DbTransaction
+  ) {}
+
   async perform (httpRequest: any): Promise<void> {
     await this.db.openTransaction()
+    await this.decoratee.perform(httpRequest)
   }
 }
 
@@ -13,13 +19,15 @@ interface DbTransaction {
 
 describe('DbTransactionController', () => {
   let db: MockProxy<DbTransaction>
+  let decoratee: MockProxy<Controller>
   let sut: DbTransactionController
   beforeAll(() => {
     db = mock()
+    decoratee = mock()
   })
 
   beforeEach(() => {
-    sut = new DbTransactionController(db)
+    sut = new DbTransactionController(decoratee, db)
   })
 
   it('should open transaction', async () => {
@@ -27,5 +35,12 @@ describe('DbTransactionController', () => {
 
     expect(db.openTransaction).toHaveBeenCalledWith()
     expect(db.openTransaction).toHaveBeenCalledTimes(1)
+  })
+
+  it('should execute decoratee', async () => {
+    await sut.perform({ any: 'any' })
+
+    expect(decoratee.perform).toHaveBeenCalledWith({ any: 'any' })
+    expect(decoratee.perform).toHaveBeenCalledTimes(1)
   })
 })
